5f7ae7aa8402abfe4f096f0d9712cbb77d7a2b2f
mrohmer <rohmer.matze@gmail.com>
2023-11-10 14:27:41 +0100

    fix: remove signup disabled message

--- src/routes/auth/login/+page.svelte
+++ src/routes/auth/login/+page.svelte
@@ -16,17 +16,6 @@
         <div class="card w-96 bg-base-100 border-primary border shadow shadow-primary">
             <div class="card-body">
                 {#if !data?.session}
-                    <div class="alert shadow-lg">
-                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
-                             class="stroke-info shrink-0 w-6 h-6">
-                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                                  d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path>
-                        </svg>
-                        <div>
-                            <h3 class="font-bold">Signups are currently disabled</h3>
-                            <div class="text-xs">Reach out to the admin of your trust to get access 😉</div>
-                        </div>
-                    </div>
                     <Auth supabaseClient={data.supabase}
                           view="magic_link"
                           redirectTo={`${url}/auth/callback`}
